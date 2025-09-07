eee9d8593a2c59b68d01269153080cbfe6d92462
Darren Miller <darren@darrenm.net>
2025-09-07 09:45:29 +0100

    Resize and convert to JPEG when serving image

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_file, abort
 from dotenv import load_dotenv
-from src import lookup
+from src import lookup, transform
+import io
+
 import os
 
 load_dotenv()
@@ -27,14 +29,23 @@ def random_photo():
     }
     return jsonify(photo_data)
 
-@app.route('/image/<path:image_path>', methods=['GET'])
-def serve_image(image_path):
+# /image/800x600/upload/b97a4f5d-0316-4b82-b46f-5e10f1a57ceb/d6/b5/d6b57617-b15a-4987-9dd7-f399266c1fb0.jpg
+@app.route('/image/<int:width>x<int:height>/<path:image_path>', methods=['GET'])
+def serve_image(width, height, image_path):
+
     image_dir = os.getenv('IMMICH_PATH')
     full_path = os.path.join(image_dir, image_path)
     if not os.path.isfile(full_path):
         abort(404)
 
-    return send_from_directory(image_dir, image_path)
+    transformer = transform.ImageTransformer(width, height)
+    image = transformer.transform(full_path)
+
+    return send_file(
+        io.BytesIO(image),
+        mimetype='image/jpeg',
+        as_attachment=False
+    )
 
 if __name__ == '__main__':
     # '0.0.0.0' makes it accessible on your local network
---
+++ src/transform.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageOps
+import io
+import pillow_heif
+
+pillow_heif.register_heif_opener()
+
+class ImageTransformer:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def transform(self, path):
+        canvas = Image.new("RGB", (self.width, self.height), (0, 0, 0))
+        imgbytes = Image.open(path).convert("RGB")
+
+        # Fit image to canvas, maintaining aspect ratio
+        img = ImageOps.contain(imgbytes, (self.width, self.height))
+        # Center the image on the canvas
+        x = (self.width - img.width) // 2
+        y = (self.height - img.height) // 2
+        canvas.paste(img, (x, y))
+        # Save to JPEG binary
+        output = io.BytesIO()
+        canvas.save(output, format="JPEG")
+        return output.getvalue()
